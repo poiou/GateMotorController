5464197685171b87c60481b5871f831f45e59d19
Marcelo Antunes (2171962) <2171962@my.ipleiria.pt>
2018-03-03 15:58:52 +0000

    Remote alredy works, and we just can open if the gate was closed

--- Code/Code/codigo.ino
+++ Code/Code/codigo.ino
@@ -1,61 +1,71 @@
 #include <RCSwitch.h>
 
+
 #define Open1 D1
 #define Close1 D2
 
 #define Open2 D3
 #define Close2 D4
 
-#define Reciever433 0
+#define Reciever433 14
 
-int ActionTime=8;
-
-int OpenCodes[] = {1500};
-
-int CloseCodes[] = {1501};
+int ActionTime=2;
+//A
+int OpenCodes[] = {7843080,13939976,10590472,2350344,3931400 };
+//B
+int CloseCodes[] = {7843076,13939972,10606852,2350340,3931396};
 
 unsigned long ActionStart=0;
 
-//0 for open adn 1 for close
-bool Action = true;
+//0 for open and 1 for close 3 for stop
+int Action = 3;
+int prevAction = 1;
 
 int RemoteSize=sizeof(OpenCodes);
 RCSwitch mySwitch = RCSwitch();
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   mySwitch.enableReceive(Reciever433);  // Receiver on inerrupt 0 => that is pin #2
   pinMode(Open1,OUTPUT);
   pinMode(Close1,OUTPUT);
   pinMode(Open2,OUTPUT);
   pinMode(Close2,OUTPUT);
   //pinMode(Reciever433,INPUT);
+  
+ digitalWrite(Close1, LOW);
+  digitalWrite(Close2, LOW);
+   digitalWrite(Open1, LOW); 
+   digitalWrite(Open2, LOW);
+
+
+  
+   Serial.begin(9600);
 }
 
 void loop() {
    
-   if((millis()/1000)==ActionStart+ActionTime){
-    Stop();
-   }
+    if((millis()/1000)==ActionStart+ActionTime && Action!=3){
+      Stop();
+           Action=3;
+  }
    if (mySwitch.available()) 
    {
     int value = mySwitch.getReceivedValue();
+        Serial.println(value);
     if (value == 0) 
     {
       Serial.print("Unknown encoding");
     } 
     else {
-
-      
-      Serial.print("Received ");
-      Serial.print( mySwitch.getReceivedValue() );
-      int code=mySwitch.getReceivedValue();
-      if(GetOpenCode(code)&Action==true){
-        Action=false;
+      if(GetOpenCode(value)&& (prevAction+Action==4)){
+        Action=3;
+        prevAction=0;
         Open();
       }
-       if(GetCloseCode(code)&Action==false){
-        Action=true;
+       if(GetCloseCode(value)&& (prevAction+Action==3)){
+        Action=3;
+        prevAction=1;
         Close();
       }
     }
@@ -68,6 +78,7 @@ void loop() {
 void Open(){
   ActionStart = millis()/1000;
   Action=false;
+  Serial.println("Open");
   digitalWrite(Close1, LOW);
   digitalWrite(Close2, LOW);
   
@@ -80,6 +91,7 @@ void Open(){
 void Close(){
   ActionStart = millis()/1000;
   Action=true;
+   Serial.println("Close");
   digitalWrite(Open1, LOW);
   digitalWrite(Open2, LOW);
   
@@ -89,8 +101,9 @@ void Close(){
 }
 
 void Stop(){
+   Serial.println("Stop");
   digitalWrite(Close1, LOW);
-  digitalWrite(Close2, HIGH);
+  digitalWrite(Close2, LOW);
   digitalWrite(Open1, LOW);
   digitalWrite(Open2, LOW);
 }
